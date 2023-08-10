3ad45ce922e988fc90ca4ac056a2df9fc6462336
Pin <apy9510@yahoo.com.tw>
2023-08-10 16:21:41 +0800

    liff api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { LiffService } from './liff.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fulun';
+export class AppComponent implements OnInit {
+  displayName = '';
+  userId = '';
+
+  constructor(private liffService: LiffService) {}
+
+  ngOnInit() {
+    this.showDisplayName();
+  }
+
+  async showDisplayName() {
+    const userData = await this.liffService.getUserData();
+    if (userData && userData.displayName) {
+      this.displayName = userData.displayName;
+      console.log('User ID:', userData.userId);
+    }
+  }
 }
---
+++ src/app/liff.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+declare var liff: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiffService {
+  private liffId: string = '1660683719-4N2ppZbQ';
+  userId: string = '';
+
+  constructor() { }
+
+  async initLIFF() {
+    try {
+      await this.loadLIFFScript();
+      await liff.init({ liffId: this.liffId });
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      }
+      console.log('LIFF API 初始化成功!');
+    } catch (error) {
+      console.error('LIFF API 初始化失敗:', error);
+    }
+  }
+
+  private loadLIFFScript() {
+    return new Promise<void>((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://static.line-scdn.net/liff/edge/2/sdk.js';
+      script.onload = () => resolve();
+      script.onerror = () => reject('Failed to load LIFF SDK');
+      document.head.appendChild(script);
+    });
+  }
+
+  async getUserData() {
+    await this.initLIFF(); // 確保初始化
+    try {
+      const profile = await liff.getProfile();
+      if (profile) {
+        return {
+          displayName: profile.displayName,
+          userId: profile.userId
+        };
+      } else {
+        return null;
+      }
+    } catch (error) {
+      //console.error('Error fetching profile:', error);
+      return null;
+    }
+  }
+
+  setUserId(userId: string) {
+    this.userId = userId;
+  }
+  getUserId() {
+    return this.userId;
+  }
+
+
+}
