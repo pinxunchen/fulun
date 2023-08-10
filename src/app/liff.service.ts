7a063a22917a49e24090583e7553adea123fd3cb
Pin <apy9510@yahoo.com.tw>
2023-08-10 17:00:11 +0800

    修改了某個功能

--- src/app/liff.service.ts
+++ src/app/liff.service.ts
@@ -10,6 +10,16 @@ export class LiffService {
 
   constructor() { }
 
+  private loadLIFFScript() {
+    return new Promise<void>((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://static.line-scdn.net/liff/edge/2/sdk.js';
+      script.onload = () => resolve();
+      script.onerror = () => reject('Failed to load LIFF SDK');
+      document.head.appendChild(script);
+    });
+  }
+
   async initLIFF() {
     try {
       await this.loadLIFFScript();
@@ -27,35 +37,6 @@ export class LiffService {
       console.error('LIFF API 初始化失敗:', error);
     }
   }
-
-  private loadLIFFScript() {
-    return new Promise<void>((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = 'https://static.line-scdn.net/liff/edge/2/sdk.js';
-      script.onload = () => resolve();
-      script.onerror = () => reject('Failed to load LIFF SDK');
-      document.head.appendChild(script);
-    });
-  }
-
-  async getUserData() {
-    await this.initLIFF(); // 確保初始化
-    try {
-      const profile = await liff.getProfile();
-      if (profile) {
-        return {
-          displayName: profile.displayName,
-          userId: profile.userId
-        };
-      } else {
-        return null;
-      }
-    } catch (error) {
-      //console.error('Error fetching profile:', error);
-      return null;
-    }
-  }
-
   setUserId(userId: string) {
     this.userId = userId;
   }
